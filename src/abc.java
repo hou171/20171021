d6a63ecdca15c5bd398941ac02fd893abdba23c2
hou171 <jimmy212tw@gmail.com>
2017-10-21 17:25:17 +0800

    大寫轉小寫

---
+++ src/abc.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class abc {
+
+    public static void main(String[]args){
+        Scanner in=new Scanner(System.in);
+        String a;
+        a=in.nextLine();
+        System.out.println(a.toLowerCase());
+    }
+}
