abd6e3b0d925618034202bedc8eb1e77f429b956
hou171 <jimmy212tw@gmail.com>
2017-10-23 12:54:36 +0800

    加密

---
+++ src/password.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class password {
+        public static void main(String []args){
+            Scanner in = new Scanner(System.in);
+            int a=in.nextInt();
+            int b,c,d,e;
+            b=(a/1000+7)%10;
+            c=(a/100%10+7)%10;
+            d=(a/10%10+7)%10;
+            e=(a%10+7)%10;
+            System.out.printf("%d%d%d%d",d,e,b,c);
+    }
+}
